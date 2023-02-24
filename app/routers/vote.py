b7c621eeb5e15244431efcfec52906edd6645bee
Ste1509 <esteffany.3801921726@ucaldas.edu.co>
2023-02-23 19:25:45 -0500

    fix tags

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -7,15 +7,14 @@ from starlette.responses import Response
 
 from .. import schemas, models, oauth2, database
 
-
 router = APIRouter(
     prefix="/vote",
-    tags="Vote"
+    tags=['Vote']
 )
 
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def vote(vote: schemas.Vote, db: Session=Depends(database.get_db),
-         current_user = Depends(oauth2.get_current_user)):
+def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user=Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
